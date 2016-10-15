05545c56b1bab00742d4336fe3b17c64a45ed130
Rafael Concha <c_rafael@correo.unimet.edu.ve>
2016-10-15 10:29:23 -0430

    prueba

---
+++ src/proyecto/pkg1/os/Proyecto1OS.java
@@ -0,0 +1,22 @@
+
+package proyecto.pkg1.os;
+
+/**
+ *
+ * Autores: Victoria Masiero y Rafael Concha
+ * Primer proyecto de sistemas operativos
+ */
+
+public class Proyecto1OS {
+
+    
+    public static void main(String[] args) {
+        
+        
+        int nombre;
+        int cedula;
+        
+        
+    }
+    
+}
